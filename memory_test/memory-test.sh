364f5c2c41214d3a51df2e43ed524cdb053fcedf
Alan Tull <atull@altera.com>
2013-06-07 10:58:03 -0500

    FogBugz #129176: add test script for DDR test
    Filling in memory-test.sh.  Returns error codes, includes online
    help.
    
    Signed-off-by: Alan Tull <atull@altera.com>

--- memory_test/memory-test.sh
+++ memory_test/memory-test.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-# todo
+usage()
+{
+    cat <<EOF
+usage: $(basename $0) [--fast]
+
+  --fast   : run a quick test, doesn't check all address lines
+
+EOF
+}
+
+#========================================================================
+if [ ! -f memtester ]; then
+    echo "Could not find memtester.  Test blocked"
+    exit 1
+fi
+
+while [ -n "$1" ]; do
+    case $1 in
+	--fast ) FAST=1 ;;
+	-h|--help ) usage ; exit 1 ;;
+	* ) echo "invalid parameter" ; echo ; usage ; exit 1 ;;
+    esac
+    shift
+done
+
+if [ "$FAST" == '1' ]; then
+    MEMSIZE=64K
+else
+    MEMSIZE=550M
+fi
+
+echo "./memtester $MEMSIZE 1"
+./memtester $MEMSIZE 1
+ret=$?
+
+echo "memtester exited with code $ret"
+
+exit $ret
