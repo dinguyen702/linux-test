a09a4adacae8c4623800b7e38f792c2ff9006ecb
graham <grmoore@altera.com>
2013-07-23 13:07:01 -0500

    FogBugz #139760: Add DMA test script
    Add script to run dmatest.
    
    Signed-off-by: graham <grmoore@altera.com>

---
+++ dma_test/dma-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dmesg -c
+dmesg -n 3
+
+insmod /lib/modules/3.7.0/kernel/drivers/dma/pl330.ko
+
+insmod /lib/modules/3.7.0/kernel/drivers/dma/dmatest.ko
+
+echo 10 >/sys/kernel/debug/dmatest/iterations
+
+echo Y >/sys/kernel/debug/dmatest/run
+
+sleep 5
+
+# success_count=`grep -c "dma0chan[0-7].*0 failures" /var/log/messages`
+success_count=`dmesg|grep -c "dma0chan[0-7].*0 failures"`
+
+echo Success count is $success_count
+if [ $success_count = "8" ]
+then
+echo "PASS"
+else
+echo "FAIL"
+fi
