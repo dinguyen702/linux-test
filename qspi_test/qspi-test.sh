be86d7b775cf0ce16bd2c74adbfad3b5af63c6fd
Graham Moore <grmoore@altera.com>
2013-07-23 15:33:50 -0500

    FogBugz #139763: Add QSPI test script
    Fix script, was not correct
    
    Signed-off-by: Graham Moore <grmoore@altera.com>

--- qspi_test/qspi-test.sh
+++ qspi_test/qspi-test.sh
@@ -1,12 +1,31 @@
-include ../Makefile.inc
+#!/bin/bash
 
-EXE =
-SCRIPTS = *.sh
+flash_erase /dev/mtd0 0 0
+#flash_erase /dev/mtd1 0 0
 
-all :
+echo Start of file >test.txt
 
-install : $(SCRIPTS) $(EXE)
-	$(INSTALL) -d $(INSTALLPATH)
-	$(INSTALL) -m755 $? $(INSTALLPATH)
+var=10000000
+while [ $var -lt 10125000 ]
+do
+   echo $var >>test.txt
+   var=$(( $var + 1 ))
+done
 
-clean :
+echo End of file >>test.txt
+
+filesize=$(ls -l test.txt |awk -F" " '{ print $5 }')
+
+echo filesize is $filesize
+
+flashcp test.txt /dev/mtd0
+mtd_debug read /dev/mtd0 0 $filesize test1.txt
+
+thesame=$(diff -q test.txt test1.txt|grep differ)
+
+if [ "$thesame" != "" ]
+then
+echo FAIL
+else
+echo PASS
+fi
