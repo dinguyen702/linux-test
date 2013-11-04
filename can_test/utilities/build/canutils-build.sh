3877b90ccc57f5d563d134d5730819d6b995cd50
Thor Thayer <tthayer@altera.com>
2013-11-04 10:08:09 -0600

    Minor changes to build scripts.

--- can_test/utilities/build/canutils-build.sh
+++ can_test/utilities/build/canutils-build.sh
@@ -2,6 +2,7 @@ rm -rf canutils-4.0.6/
 tar xfj canutils-4.0.6.tar.bz2
 chmod -R 777 canutils-4.0.6
 cd canutils-4.0.6
+export PATH=/opt/altera-linux/linaro/gcc-linaro-arm-linux-gnueabihf-4.7-2012.11-20121123_linux/bin/:$PATH
 ./configure --host=arm-linux-gnueabihf --prefix=$PWD/__install \
 libsocketcan_LIBS=-lsocketcan \
 LDFLAGS=-L$PWD/../libsocketcan-0.0.9/__install/lib \
