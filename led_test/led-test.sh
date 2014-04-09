53bf3d8758d708816e301df38774f3aba168efb1
Alan Tull <atull@altera.com>
2014-04-09 11:10:10 -0500

    led-test improvements
    Wiggle all leds under /sys/class/leds
    
    Signed-off-by: Alan Tull <atull@altera.com>

--- led_test/led-test.sh
+++ led_test/led-test.sh
@@ -2,11 +2,12 @@
 
 SLEEP_TIME=1
 SYS_LEDS=/sys/class/leds/
+LEDS="$(ls $SYS_LEDS)"
 
 usage()
 {
     cat <<EOF
-Test will blink some leds.
+Test will find all leds under $SYS_LEDS and blink them.
 
 Usage: $(basename $0) [-h]
 
@@ -22,20 +23,17 @@ delay_for_user()
     fi
 }
 
-# Param 1 = led # (0, 1, 2, 3)
-# Param 2 = 0 for off; 1 for on
+# Param 1 = led ; Param 2 = 1 for on, 0 for off
 set_brightness()
 {
     echo "setting led $1 to $2"
-    led_brightness=/sys/class/leds/hps_led${1}/brightness
+    led_brightness=/sys/class/leds/${1}/brightness
     echo $2 > $led_brightness
     bright="$(cat $led_brightness)"
     if [ "$2" != "$bright" ]; then
 	echo "ERROR : brightness read back != brightness set"
     fi
 }
-
-#=============================================================
 
 while [ -n "$1" ]; do
     case $1 in
@@ -45,15 +43,18 @@ while [ -n "$1" ]; do
     shift
 done
 
-#=============================================================
-for led in 0 1 2 3; do
+for led in $LEDS; do
     set_brightness $led 0
 done
+echo
+delay_for_user
+
 for onoff in 1 0 1; do
-    for led in 0 1 2 3; do
-	set_brightness $led $onoff
+    for led in $LEDS; do
+        set_brightness $led $onoff
         delay_for_user
     done
+    echo
 done
 
 echo "Done!"
