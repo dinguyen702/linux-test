d22e3b3985d6d672d9531d7c2fdc661d1e9ddefa
Vince Bridgers <vbridger@opensource.altera.com>
2014-10-02 15:37:15 -0500

    Add expect script baseline for developer unit tests
    Signed-off-by: Vince Bridgers <vbridger@opensource.altera.com>

---
+++ preloader-uboot/bash-scripts/nbreboot.py
@@ -0,0 +1,30 @@
+import getpass
+import sys
+import telnetlib
+import datetime
+import time
+
+ipaddr = sys.argv[1]
+nbport = sys.argv[2]
+
+print 'Telnet to Netbooter ', ipaddr, ' and cycle port on port ', nbport
+
+# $A4 tells the netbooter to reboot
+rebootstr = '$A4 ' + nbport + '\r'
+
+tn = telnetlib.Telnet(ipaddr);
+
+tn.read_until('>')
+time.sleep(1)
+
+tn.write(rebootstr)
+
+time.sleep(1)
+
+data = ''
+while data.find('>') == -1:
+	data = tn.read_very_eager()
+
+print data
+
+tn.close()
