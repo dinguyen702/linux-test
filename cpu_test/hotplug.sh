54199921038d4a05c05c2718eb2a3489a5e6520a
Yves Vandervennet <yvanderv@opensource.altera.com>
2014-10-22 16:36:38 -0500

    Enhanced SMP test.
    V6:
      + removed an unused variable (online)
      + fixed a tiny bug in is_cpu_online()
    
    V5:
      + simplified the use of is_cpu_online()
      + removed unused code
      - did not change the get_rnd_cmd()
    
    V4:
      + changed MACHINE to use /proc/device-tree/model
    
    V3:
      + simplified cpu_is_present()
      + removed unused file 'smp_test.sh'
    
    V2:
      + improves regex to search CPU's from /proc/cpuinfo
      + changed 'get_cpu' to 'cpu_is_present'
      + removed unecessary comments
      + specifies the use of bash in libcpu.sh
      + removed unused function, 'smp_check'
      + increased the number of iterations to 10,000
      + added constants for values used at various spots in the script
      + removed the test case for CPU 0 in shutdown_cpu and bringup_cpu, as
        the kernel should safeguard against this

--- cpu_test/hotplug.sh
+++ cpu_test/hotplug.sh
@@ -1,102 +1,162 @@
 #!/bin/bash
 
-get_cpu0()
-{
-   cat /proc/cpuinfo | cut -c-13 | grep processor | grep 0 | cut -d ':' -f 2
+# internals
+SELF=$(basename $0)
+SELF_DIR=$(dirname $0)
+CONST_CMD_CPU_UP="bringup_cpu"
+CONST_CMD_CPU_DOWN="shutdown_cpu"
+
+CONST_TEST_SUCCESS="success"
+CONST_TEST_FAILURE="failure"
+
+# test specific
+ITERATIONS=10000              
+
+declare -a CPU_STATE
+
+# library of functions
+source ${SELF_DIR}/libcpu.sh
+
+init_env() {
+
+     local soc_cpu_list=$(get_soc_cpu_list)
+     local cpu
+     local cpu_count=0
+
+     for cpu in ${soc_cpu_list} ; do
+     
+         CPU_STATE[${cpu}]=$(is_cpu_online ${cpu})
+         if [ ${CPU_STATE[${cpu}]} -eq 0 ] ; then
+             cpu_count=$((${cpu_count} + 1))
+         fi
+     done
+
+     if  [ ${cpu_count} -ne $(get_num_expected_cpus) ] ; then
+         #  given the kernel command line, there's a mismatch in #
+         #  of expected CPU's being online
+        echo "${FUNCNAME}: error: mismatch in number of expected CPU's"
+        return 1
+     fi
+
+     echo "${FUNCNAME}: info: found ${cpu_count} CPU's being online, OK"
+
+     return 0
 }
 
-get_cpu1()
-{
-   cat /proc/cpuinfo | cut -c-13 | grep processor | grep 1 | cut -d ':' -f 2
+#  command is either ${CONST_CMD_CPU_DOWN} or ${CONST_CMD_CPU_UP}
+#  command names returned are actual function names
+function get_rand_cmd() {
+
+    local rnd=$(( ${RANDOM} % 2))
+
+    if [ ${rnd} -eq 0 ] ; then
+        echo ${CONST_CMD_CPU_DOWN}
+    else
+        echo ${CONST_CMD_CPU_UP}
+    fi
+    
+    return 0
 }
 
-shutdown_cpu1()
-{
-   echo 0 > /sys/devices/system/cpu/cpu1/online
+#  get a random CPU from the list of the SoC's possible CPU's
+#  which helps cover negative test cases
+function get_rand_cpu() {
+
+    local rnd=$(( ${RANDOM} % $(get_soc_max_num_cpus) ))
+
+    echo ${rnd}
+
+    return 0
 }
 
-bringup_cpu1()
-{
-   echo 1 >  /sys/devices/system/cpu/cpu1/online
+#  assumption: all available CPU's (per kernel command line) are online
+#  before we start this function
+#  covers positive and negative cases
+hotplug_test() {
+
+    local i=0
+    local cpu
+    local cmd
+    local max_cpu=$(get_num_expected_cpus)  # highest index of this runtime
+    local expected_result
+    local result
+    local err=0
+
+    echo "${FUNCNAME}: info: running hotplug test ${IERATIONS} times"
+
+    # repeat ${ITERATIONS} times
+    while [ ${i} -lt ${ITERATIONS} ] ; do
+        cmd=$(get_rand_cmd)
+        cpu=$(get_rand_cpu)
+
+        # Update the expected CPU state array
+        case ${cmd} in
+            ${CONST_CMD_CPU_DOWN})
+                      CPU_STATE[${cpu}]=0
+                      ;;
+            ${CONST_CMD_CPU_UP})
+                      CPU_STATE[${cpu}]=1
+		      ;;
+        esac
+
+        # if the cpu # picked is higher than max_cpu,
+        # then we know failure to execute ${cmd} is expected
+        if [ ${cpu} -gt ${max_cpu} ] ; then
+             expected_result="${CONST_TEST_FAILURE}"
+        else
+             if [ ${cpu} -eq 0 -a "${cmd}" == "${CONST_CMD_CPU_DOWN}" ] ; then
+                 expected_result="${CONST_TEST_FAILURE}"
+             else 
+                 expected_result="${CONST_TEST_SUCCESS}"
+             fi   
+        fi
+
+        # let's execute ${cmd}
+        ${cmd} ${cpu}
+        result=$?
+        if [ ${result} -eq 0 ] ; then
+            if [ ${expected_result} == "${CONST_TEST_SUCCESS}" ] ; then
+                echo "${FUNCNAME}: info: ${i}: ${cpu}: ${cmd}: OK"
+            else 
+                echo "${FUNCNAME}: info: ${i}: ${cpu}: ${cmd}: FFFF"
+                err=1
+            fi 
+        else 
+            if [ ${expected_result} == "${CONST_TEST_SUCCESS}" ] ; then
+                echo "${FUNCNAME}: info: ${i}: ${cpu}: ${cmd}: FFFF"
+                err=1
+            else 
+                echo "${FUNCNAME}: info: ${i}: ${cpu}: ${cmd}: OK"
+            fi 
+        fi 
+
+        # keep on testing
+        i=$(( ${i} + 1 ))
+    done
+
+    if [ ${err} -gt 0 ] ; then
+        echo "${FUNCNAME}: info: test failed"
+    else
+        echo "${FUNCNAME}: info: test success"
+    fi
+
+    return ${err}
 }
 
-smp_test()
-{
-   echo "Looking for CPU 0"
-   cpu0_info="$(get_cpu0)"
 
-   echo "CPU0: $cpu0_info"
-   if [ "$cpu0_info" -ne "$CPU0_INFO" ]; then
-	echo "Failed to see CPU0. Expecting $CPU0_INFO"
-	status_fail=1
-	return
-   fi
-
-   echo "Looking for CPU 1"
-   cpu1_info="$(get_cpu1)"
-
-   echo "CPU1: $cpu1_info"
-   if [ -z "$cpu1_info" ]; then
-	echo "Failed to see CPU1. Expecting $CPU1_INFO"
-	status_fail=1
-	return
-   fi
-}
-
-hotplug_test()
-{
-   echo "Unplugging CPU1"
-
-   shutdown_cpu1
-
-   echo "Looking for CPU 1"
-   cpu1_info="$(get_cpu1)"
-   echo "CPU1: $cpu1_info"
-   if [ -z "$cpu1_info" ]; then
-	echo "Failed to see CPU1. CORRECT!"
-	status_fail=0
-   else
-	status_fail=1
-	return
-   fi
-
-   bringup_cpu1
-
-   echo "Looking for CPU 1"
-   cpu1_info="$(get_cpu1)"
-
-   echo "CPU1: $cpu1_info"
-   if [ -z "$cpu1_info" ]; then
-	echo "Failed to see CPU1. Expecting $CPU1_INFO"
-	status_fail=1
-	return
-   fi
-}
-
-
-CPU0_INFO=0
-CPU1_INFO=1
-DEVNODE=/proc/cpuinfo
-status_fail=0
-
-cpu1_shutdown_str='CPU1: shutdown'
-#===========================================================
-echo "HOTPLUG test - Dynamically shutdown CPU1"
-echo
-
-if [ -z "$DEVNODE" ] || [ ! -e "$DEVNODE" ]; then
-    echo "Error cannot find devnode $DEVNODE"
+echo "${SELF}: info: init'ing env..."
+init_env
+if [ $? -ne 0 ] ; then
+    echo "${SELF}: init_env: failed"
     exit 1
 fi
 
-smp_test
+echo "${SELF}: info: starting hotplug test..."
 hotplug_test
-
-echo
-if [ "$status_fail" == 0 ]; then
-    echo "PASS - Able to hotplug CPU1"
-else
-    echo "FAIL - NOT able to hotplug CPU1"
+if [ $? -ne 0 ] ; then
+    echo "${SELF}: hotplug_test: failed"
+    exit 1
 fi
 
-exit $status_fail
+exit 0
+
