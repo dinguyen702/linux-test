dac6ba790d9c97116eded0eefc391fe031c3cc68
Graham Moore <grmoore@opensource.altera.com>
2015-06-23 09:46:09 -0500

    Improve qspi-test
    Signed-off-by: Graham Moore <grmoore@opensource.altera.com>

--- qspi_test/qspi-test.sh
+++ qspi_test/qspi-test.sh
@@ -8,12 +8,10 @@ filesize=$(ls -l $filename |awk -F" " '{ print $5 }')
 echo filesize is $filesize
 
 flashcp $filename /dev/mtd0
+rm qspi-data-out
 mtd_debug read /dev/mtd0 0 $filesize qspi-data-out
 
-thesame=$(diff -q $filename qspi-data-out|grep differ)
-
-if [ "$thesame" != "" ]
-then
+if ! diff -q $filename qspi-data-out; then
 echo FAIL
 status=1
 else
