314a2af3ed09fb7538aedcdf2f8d0635d6d7535e
Thor Thayer <thor.thayer@linux.intel.com>
2017-01-20 16:28:13 -0600

    Update ECC Memory Tests with new ECC test functions
    Use the new ECC test functions for the Memory blocks
    (DDR, L2 cache, OCRAM).
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>
    ---
    v2 Change contains_element() to shift off first variable as
       needle and then treat other parameters as one large array.
       Only call contains_element() once but pass both arrays.

--- ecc_test/ecc_test.sh
+++ ecc_test/ecc_test.sh
@@ -6,6 +6,13 @@ ECC_STATUS_PATH_TOP=/sys/devices/system/edac
 SELF="$(basename $0)"
 DIRSELF="$(dirname $0)"
 
+# ------------ Main Memory ECCs ------------------------------
+declare -a MEMORY_ECC_PERIPHERALS=()
+
+declare -ar CA5_MEMORY_ECC_PERIPHS=("ddr" "l2" "ocram")
+declare -ar A10_MEMORY_ECC_PERIPHS=("ddr" "l2" "ocram")
+
+# ------------ FIFO ECCs ------------------------------
 declare -a FIFO_ECC_PERIPHERALS=()
 
 declare -ar A10_FIFO_ECC_PERIPHS=("usb0" "usb1" "qspi" "nand" "dma" "emac0-rx" \
@@ -29,8 +36,8 @@ function get_devkit_type()
 # works with empty arrays.
 function contains_element()
 {
-	local needle="$1"
-	declare -a haystack=("${!2}")
+	local needle="$1"; shift;
+	declare -a haystack=("$@")
 
 	if [ "$( echo ${haystack[@]} | grep -w -o ${needle} | wc -l )" -gt "0" ]; then
 		return 0
@@ -45,19 +52,29 @@ function contains_element()
 # $3 = Peripheral instance (ie usb0-ecc0 for USB0's FIFO ECC)
 function ecc_test::inject_and_check_errors() {
 	local ecc_name="$1"
+	local skip_ecc="$2"
 	local ecc_dir="${ecc_name}-ecc"
+	# DDR is special and has a memory controller designation.
+	if [ "${ecc_name}" == "ddr" ]; then
+		ecc_dir="mc"
+	fi
 	local err_status_path_root="${ECC_STATUS_PATH_TOP}/${ecc_dir}"
 	local ecc_instance="${3:-0}"
 	local err_status_path="${err_status_path_root}/${ecc_dir}${ecc_instance}"
+	# DDR is special and has a memory controller designation with instance.
+	if [ "${ecc_name}" == "ddr" ]; then
+		ecc_dir="mc0"
+	fi
 	local err_inject_path="/sys/kernel/debug/edac/${ecc_dir}/altr_trigger"
 
 	# Check allowed list of peripherals. Quit if not valid
-	if ( contains_element "{ecc_name}" "${FIFO_ECC_PERIPHERALS[@]}" ); then
+	if ( contains_element "{ecc_name}" "${MEMORY_ECC_PERIPHERALS[@]}" \
+				"${FIFO_ECC_PERIPHERALS[@]}" ); then
 		echo "ERROR: ${FUNCNAME} => invalid ECC ${ecc_name}"
 		status_fail=1
 		return
 	fi
-	echo "Peripheral is $1"
+	echo "ECC Peripheral is $1"
 	#echo "Status path is ${err_status_path}"
 
 	if [ -d ${err_status_path_root} ]; then
@@ -68,7 +85,7 @@ function ecc_test::inject_and_check_errors() {
 		sleep 1
 
 		echo 'C' > ${err_inject_path}
-		if [ $2 -eq 0 ]; then
+		if [ ${skip_ecc} -eq 0 ]; then
 			echo 'U' > ${err_inject_path}
 		fi
 
@@ -77,11 +94,19 @@ function ecc_test::inject_and_check_errors() {
 
 		#echo; echo "${ecc_name^^} ECC TEST RESULT:"
 		# OCRAM, SDRAM, and L2 cache will panic on Uncorrectable errors.
-		if [ ${start_cerrs} -ne ${end_cerrs} ] &&
-		   [ ${start_uerrs} -ne ${end_uerrs} ]; then
-			compare_result="PASS"
+		if [ ${skip_ecc} -eq 0 ]; then
+			if [ ${start_cerrs} -ne ${end_cerrs} ] &&
+			   [ ${start_uerrs} -ne ${end_uerrs} ]; then
+				compare_result="PASS"
+			else
+				status_fail=1
+			fi
 		else
-			status_fail=1
+			if [ ${start_cerrs} -ne ${end_cerrs} ]; then
+				compare_result="PASS"
+			else
+				status_fail=1
+			fi
 		fi
 		# Accumulate the results for summarizing later
 		ecc_err_accounting[${index}]=$1
@@ -104,6 +129,22 @@ echo "---------------------------------------------------------"
 
 SOC="$(get_devkit_type)"
 
+#------------------- Main Memory Tests ------------------------------
+case ${SOC} in
+	ArriaV|CycloneV) MEMORY_ECC_PERIPHERALS=( "${CA5_MEMORY_ECC_PERIPHS[@]}" ) ;;
+	Arria10) MEMORY_ECC_PERIPHERALS=( "${A10_MEMORY_ECC_PERIPHS[@]}" ) ;;
+	*) echo "Unsupported SoC (${SOC})"; exit 1 ;;
+esac
+
+# Cycle through all the different Memories.
+for ecc_arg in "${MEMORY_ECC_PERIPHERALS[@]}"
+do
+	ecc_test::inject_and_check_errors ${ecc_arg} 1
+	echo "---------------------------------------------------------"
+done
+echo
+
+#----------------- Peripheral FIFO Tests ----------------------------
 case ${SOC} in
 	ArriaV|CycloneV) true ;;
 	# Only Arria10 supports FIFO ECCs right now.
@@ -121,7 +162,8 @@ echo
 # Short delay to allow any deferred UE errors to print.
 sleep 2
 
-# Print the Peripheral ECC summary results
+#----------------- Print the ECC results ----------------------------
+# Print the ECC summary results
 element_count=${#ecc_err_accounting[@]}
 index=0
 echo "ECC Test Summary (${SELF})"
@@ -137,134 +179,11 @@ do
 	index=$((index+6))
 done
 
-# First read all the ce_counts
-if [ -e /sys/devices/system/edac/mc/mc0/ce_count ]; then
-	echo "Read # of Correctable Errors on SDRAM Mem Controller"
-	cat /sys/devices/system/edac/mc/mc0/ce_count
-	cat /sys/devices/system/edac/mc/mc0/ce_count > sdram_cecount_orig.txt
-fi
-if [ -e /sys/devices/system/edac/l2edac/l2edac0/ce_count ]; then
-	echo "Read # of Correctable Errors on L2 Cache"
-	cat /sys/devices/system/edac/l2edac/l2edac0/ce_count
-	cat /sys/devices/system/edac/l2edac/l2edac0/ce_count > l2_cecount_orig.txt
-fi
-if [ -e /sys/devices/system/edac/ocramedac/ocramedac0/ce_count ]; then
-	echo "Read # of Correctable Errors on OCRAM"
-	cat /sys/devices/system/edac/ocramedac/ocramedac0/ce_count
-	cat /sys/devices/system/edac/ocramedac/ocramedac0/ce_count > ocram_cecount_orig.txt
-fi
-
-# Now perform at least 1 bit error on each.
-echo; echo;
-if [ -e /sys/devices/system/edac/mc/mc0/ce_count ]; then
-	echo "Inject a correctable error into SDRAM"
-	echo 1 > /sys/kernel/debug/edac/mc0/inject_ctrl
-	sleep 1;
-fi
-if [ -e /sys/devices/system/edac/l2edac/l2edac0/ce_count ]; then
-	echo "Inject a correctable error into L2 cache"
-	echo 1 > /sys/devices/system/edac/l2edac/altr_l2_trigger
-	echo 1 > /sys/devices/system/edac/l2edac/altr_l2_trigger
-	sleep 1;
-fi
-if [ -e /sys/devices/system/edac/ocramedac/ocramedac0/ce_count ]; then
-	echo "Inject a correctable error into OCRAM"
-	echo 1 > /sys/devices/system/edac/ocramedac/altr_ocram_trigger
-	sleep 1;
-fi
-
-# Repeat reads.
-echo; echo
-if [ -e /sys/devices/system/edac/mc/mc0/ce_count ]; then
-	echo "Read # of Correctable Errors on SDRAM Mem Controller"
-	cat /sys/devices/system/edac/mc/mc0/ce_count
-	cat /sys/devices/system/edac/mc/mc0/ce_count > sdram_cecount_new.txt
-fi
-if [ -e /sys/devices/system/edac/l2edac/l2edac0/ce_count ]; then
-	echo "Read # of Correctable Errors on L2 Cache"
-	cat /sys/devices/system/edac/l2edac/l2edac0/ce_count
-	cat /sys/devices/system/edac/l2edac/l2edac0/ce_count > l2_cecount_new.txt
-fi
-if [ -e /sys/devices/system/edac/ocramedac/ocramedac0/ce_count ]; then
-	echo "Read # of Correctable Errors on OCRAM"
-	cat /sys/devices/system/edac/ocramedac/ocramedac0/ce_count
-	cat /sys/devices/system/edac/ocramedac/ocramedac0/ce_count > ocram_cecount_new.txt
-fi
-
-if test ! -e sdram_cecount_new.txt ; then
-	echo "SDRAM ECC did not work - check to make sure it is enabled"
-        status_fail=1
-fi
-if test ! -e l2_cecount_new.txt ; then
-	echo "L2 ECC did not work - check to make sure it is enabled"
-        status_fail=1
-fi
-if test ! -e ocram_cecount_new.txt ; then
-	echo "OCRAM ECC did not work - check to make sure it is enabled"
-        status_fail=1
-fi
-
-if [ "$status_fail" != '1' ]; then
-   echo "Compare the DDR initial & ecc triggered files"
-   CMD="diff sdram_cecount_orig.txt sdram_cecount_new.txt"
-   echo "$CMD"
-   $CMD
-   ret=$?
-
-   if [ "$ret" == '0' ]; then
-        echo "FAIL - return code is $ret"
-        status_fail=1
-   fi
-
-   echo "Compare the l2 initial & ecc triggered files"
-   CMD1="diff l2_cecount_orig.txt l2_cecount_new.txt"
-   echo "$CMD1"
-   $CMD1
-   ret1=$?
-
-   if [ "$ret1" == '0' ]; then
-        echo "FAIL - return code is $ret1"
-        status_fail=1
-   fi
-   
-   echo "Compare the OCRAM initial & ecc triggered files"
-   CMD2="diff ocram_cecount_orig.txt ocram_cecount_new.txt"
-   echo "$CMD2"
-   $CMD2
-   ret2=$?
-
-   if [ "$ret2" == '0' ]; then
-        echo "FAIL - return code is $ret2"
-        status_fail=1
-   fi
-fi
-
-# Cleanup
-if [ -e sdram_cecount_orig.txt ]; then
-	rm sdram_cecount_orig.txt
-fi
-if [ -e sdram_cecount_new.txt ]; then
-	rm sdram_cecount_new.txt
-fi
-if [ -e l2_cecount_orig.txt ]; then
-	rm l2_cecount_orig.txt
-fi
-if [ -e l2_cecount_new.txt ]; then
-	rm l2_cecount_new.txt
-fi
-if [ -e ocram_cecount_orig.txt ]; then
-	rm ocram_cecount_orig.txt
-fi
-if [ -e ocram_cecount_new.txt ]; then
-	rm ocram_cecount_new.txt
-fi
-
 echo
 if [ "$status_fail" == 0 ]; then
-    echo "PASS"
+	echo "TEST PASSED"
 else
-    echo "FAIL due to failures already listed above"
+	echo "TEST FAILED due to failures already listed above"
 fi
 
 exit $status_fail
-
