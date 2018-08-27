d36eb2ed1b89dbeefb5fc1f91d1ff59f0f019d80
Thor Thayer <thor.thayer@linux.intel.com>
2018-08-27 13:12:18 -0500

    Improvements to QSPI MTD tests
    Only install kernel modules if
    1) kernel module is not already installed,
    2) kernel module is available
    
    Since writing to wrong partition in Arria10 erases the U-Boot,
    protect against this by checking which CPU is running using
    DEV_PARTITION.
    
    Add dump of dmesg for recording the mtd results.
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>
    ---
    v4  Replace all cases of - with _
        Use "$@" to grab all parameters in cleanup_modules()
    v3  Change to a loop to install and then
        uninstall modules.
    v2  Set QSPI partition to 1 for Stratix10.
        Wrap module path and arguments in quotes.
        Remove tail from dmesg
        Use modprobe -r to take care of dependencies.

--- qspi_test/qspi-mtd-tests.sh
+++ qspi_test/qspi-mtd-tests.sh
@@ -1,27 +1,89 @@
 #!/bin/bash
 
-if ! insmod spi-nor.ko
-then
-exit
-fi
+MODULE_DIR="/lib/modules/$(uname -r)/kernel/drivers/mtd"
+CHECK_PATH="${MODULE_DIR}/spi-nor/"
+declare -a DEV_PARTITION=0
+declare -x LOADED_MODULES=""
 
-if ! insmod cadence-quadspi.ko
-then
-exit
-fi
+# TODO: Put this in a shared library function. Currently used in spi_test & ecc_test.
+function get_devkit_type()
+{
+	# Altera SOCFPGA Arria V SoC Development Kit   ==> ArriaV
+	# Altera SOCFPGA Cyclone V SoC Development Kit ==> CycloneV
+	# Altera SOCFPGA Arria 10 ==> Arria10
+	# SoCFPGA Stratix 10 SoCDK
+	cat /proc/device-tree/model | sed 's/Altera //' | cut -d ' ' -f 2-3 | tr -d ' '
+}
 
-if ! insmod mtd_speedtest.ko dev=0
-then 
-exit
-fi
+# TODO: Put this in a shared library function. Currently used in qspi_test but maybe dma.
+function cleanup_modules()
+{
+	loaded_modules="$@"
+	echo "Unravel the installed modules."
+	for module in ${loaded_modules}; do
+		MODULE_NAME="${module//-/_}"
+		echo "Removing ${MODULE_NAME}"
+		if ! modprobe -r ${MODULE_NAME}; then
+			echo "!! Unable to remove module[${MODULE_NAME}]."
+		fi
+	done
+}
 
-if ! insmod mtd_readtest.ko dev=0
-then
-exit
-fi
+SOC="$(get_devkit_type)"
+case ${SOC} in
+	ArriaV|CycloneV) DEV_PARTITION=0 ;;
+	# Arria10 has the boot code in partition0 so don't erase it.
+	Arria10) DEV_PARTITION=1 ;;
+	Stratix10) DEV_PARTITION=1 ;;
+	*) echo "Unsupported SoC (${SOC})"; exit 1 ;;
+esac
 
-if ! insmod mtd_stresstest.ko dev=0
-then
-exit
-fi
+#Clear the log because we read the log at the end.
+dmesg -c 2>&1 > /dev/null
+
+echo "Check for installed modules"
+
+for module in spi-nor cadence-quadspi ; do
+	# Check to see if the module is already installed and if the kernel module
+	# is available. Install it if not installed already.
+	MODULE_NAME="${module/-/_}"
+	lsmod | grep -q "${MODULE_NAME}"
+	INST_RET_CODE=$?
+	echo "Retcode is ${INST_RET_CODE}"
+	if [ ${INST_RET_CODE} -ne 0  -a -e "${CHECK_PATH}/${module}.ko" ] ; then
+		echo "install ${module} module"
+		if ! insmod "${MODULE_DIR}/spi-nor/${module}.ko" ; then
+			echo "Problem installing ${module}. Exiting..."
+			cleanup_modules "${LOADED_MODULES}"
+			exit -1
+		fi
+	else
+		echo "The ${module} module is already installed."
+	fi
+	LOADED_MODULES="${module} ${LOADED_MODULES}"
+done
+
+echo "Using parition ${DEV_PARTITION} for ${SOC}"
+echo
+echo "Beginning Tests..."
+echo
+
+for module in mtd_speedtest mtd_readtest mtd_stresstest; do
+	echo "Run Test: ${module}"
+	if ! insmod "${MODULE_DIR}/tests/${module}.ko" "dev=${DEV_PARTITION}"
+	then 
+		echo "Problem running ${module}. Exiting..."
+		cleanup_modules "${LOADED_MODULES}"
+		exit -1
+	fi
+	LOADED_MODULES="${module} ${LOADED_MODULES}"
+done
+
+echo "Test Results"
+
+dmesg
+
+cleanup_modules "${LOADED_MODULES}"
+
+echo "MTD Tests complete"
 
--- qspi_test/qspi-test.sh
+++ qspi_test/qspi-test.sh
@@ -1,15 +1,72 @@
 #!/bin/bash
 
-flash_erase /dev/mtd0 0 0
+# TODO: Add the MODULE_DIR to the shared library.
+MODULE_DIR="/lib/modules/$(uname -r)/kernel/drivers/mtd"
+CHECK_PATH="${MODULE_DIR}/spi-nor/"
+declare -a DEV_PARTITION=0
+
+# TODO: Put this in a shared library function. Currently used in spi_test & ecc_test.
+function get_devkit_type()
+{
+	# Altera SOCFPGA Arria V SoC Development Kit   ==> ArriaV
+	# Altera SOCFPGA Cyclone V SoC Development Kit ==> CycloneV
+	# Altera SOCFPGA Arria 10 ==> Arria10
+	# SoCFPGA Stratix 10 SoCDK
+	cat /proc/device-tree/model | sed 's/Altera //' | cut -d ' ' -f 2-3 | tr -d ' '
+}
+
+SOC="$(get_devkit_type)"
+case ${SOC} in
+	ArriaV|CycloneV) DEV_PARTITION=0 ;;
+	# Arria10 has the boot code in partition0 so don't erase it.
+	Arria10) DEV_PARTITION=1 ;;
+	Stratix10) DEV_PARTITION=1 ;;
+	*) echo "Unsupported SoC (${SOC})"; exit 1 ;;
+esac
+
+echo "Check for installed modules"
+
+# Check to see if the module is already installed and if the kernel module
+# is available. Install it if not installed already.
+lsmod | grep -q 'spi_nor'
+INST_RET_CODE=$?
+echo "Retcode is ${INST_RET_CODE}"
+if [ ${INST_RET_CODE} -ne 0  -a -e "${CHECK_PATH}/spi-nor.ko" ] ; then
+	echo "install spi-nor module"
+	if ! insmod "${MODULE_DIR}/spi-nor/spi-nor.ko"; then
+		exit
+	fi
+else
+	echo "The spi-nor module is already installed."
+fi
+
+# Check to see if the module is already installed and if the kernel module
+# is available. Install it if not installed already.
+lsmod | grep -q 'cadence_quadspi'
+INST_RET_CODE=$?
+echo "Retcode is ${INST_RET_CODE}"
+if [ ${INST_RET_CODE} -ne 0 -a -e "${CHECK_PATH}/cadence-quadspi.ko" ] ; then
+	echo "Install cadence_quadspi module"
+	if ! insmod "${MODULE_DIR}/spi-nor/cadence-quadspi.ko" ; then
+		exit
+	fi
+else
+	echo "The cadence_quadspi module is already installed."
+fi
+
+echo "Operating on partition ${DEV_PARTITION} for ${SOC}"
+MTD_DEV="mtd${DEV_PARTITION}"
+
+flash_erase "/dev/${MTD_DEV}" 0 0
 
 filename=qspi-data-file
 filesize=$(ls -l $filename |awk -F" " '{ print $5 }')
 
 echo filesize is $filesize
 
-flashcp $filename /dev/mtd0
+flashcp $filename "/dev/${MTD_DEV}"
 rm qspi-data-out
-mtd_debug read /dev/mtd0 0 $filesize qspi-data-out
+mtd_debug read "/dev/${MTD_DEV}" 0 $filesize qspi-data-out
 
 if ! diff -q $filename qspi-data-out; then
 echo FAIL
